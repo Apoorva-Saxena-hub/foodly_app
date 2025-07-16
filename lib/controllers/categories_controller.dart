1a832b8a4e871fd158065ca939d0198c54b357af
Apoorva-Saxena-hub <saxenapoorva1307@gmail.com>
2025-07-16 08:55:05 +0530

    changes in background image

--- lib/controllers/categories_controller.dart
+++ lib/controllers/categories_controller.dart
@@ -1,6 +1,5 @@
 import 'package:get/get_rx/src/rx_types/rx_types.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
-import 'package:get_storage/get_storage.dart';
 
 class CategoriesController extends GetxController {
   RxString _category = ''.obs;
--- lib/views/category/allCategories.dart
+++ lib/views/category/allCategories.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:foodly_app/common/app_style.dart';
+import 'package:foodly_app/common/background_container.dart';
 import 'package:foodly_app/common/reusable_text.dart';
 import 'package:foodly_app/constants/constants.dart';
 import 'package:foodly_app/constants/uidata.dart';
-import 'package:foodly_app/views/home/widgets/widgets.dart';
 
 class Allcategories extends StatelessWidget {
   const Allcategories({super.key});
@@ -15,22 +15,43 @@ class Allcategories extends StatelessWidget {
       appBar: AppBar(
         elevation: 0,
         backgroundColor: kOffWhite,
-        title: ReusableText(
-          text: "All Categories",
-          style: appStyle(12, kGray, FontWeight.w600),
+        title: Center(
+          child: ReusableText(
+            text: "Categories",
+            style: appStyle(15, kGray, FontWeight.w600),
+          ),
         ),
       ),
-      body: Container(
-        padding: EdgeInsets.only(left: 12.w, top: 10.h),
-        height: height,
-        child: ListView(
-          scrollDirection: Axis.horizontal,
-          children: List.generate(categories.length, (i) {
-            var category = categories[i];
-            return ListTile(
-              leading: CircleAvatar(radius: 18.r, backgroundColor: kGrayLight),
-            );
-          }),
+      body: BackgroundContainer(
+        color: Colors.white,
+        child: Container(
+          padding: EdgeInsets.only(left: 12.w, top: 10.h),
+          height: height,
+          child: ListView(
+            scrollDirection: Axis.vertical,
+            children: List.generate(categories.length, (i) {
+              var category = categories[i];
+              return ListTile(
+                leading: CircleAvatar(
+                  radius: 18.r,
+                  backgroundColor: kGrayLight,
+                  child: Image.network(
+                    category["imageUrl"],
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                title: ReusableText(
+                  text: category['title'],
+                  style: appStyle(12, kGray, FontWeight.normal),
+                ),
+                trailing: Icon(
+                  Icons.arrow_forward_ios_rounded,
+                  color: kGray,
+                  size: 15.r,
+                ),
+              );
+            }),
+          ),
         ),
       ),
     );
