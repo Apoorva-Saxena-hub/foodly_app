73c69a86a70551bf31b43c2f48e8292aca7490b5
Apoorva-Saxena-hub <saxenapoorva1307@gmail.com>
2025-07-17 12:12:29 +0530

    changes in recommendation ui

--- lib/views/home/allfastestfoods.dart
+++ lib/views/home/allfastestfoods.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:foodly_app/common/app_style.dart';
+import 'package:foodly_app/common/background_container.dart';
 import 'package:foodly_app/common/reusable_text.dart';
 import 'package:foodly_app/constants/constants.dart';
+import 'package:foodly_app/constants/uidata.dart';
+import 'package:foodly_app/views/home/widgets/food_tile.dart';
 
 class AllFastestFoods extends StatelessWidget {
   const AllFastestFoods({super.key});
@@ -9,15 +13,28 @@ class AllFastestFoods extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: kSecondary,
       appBar: AppBar(
         elevation: 0.3,
-        backgroundColor: kOffWhite,
+        backgroundColor: kSecondary,
         title: ReusableText(
           text: "All fastest foods",
           style: appStyle(13, kGray, FontWeight.w600),
         ),
       ),
-      body: const Center(child: Text("All fastest foods")),
+      body: BackgroundContainer(
+        color: kLightWhite,
+        child: Padding(
+          padding: EdgeInsets.all(12.h),
+          child: ListView(
+            scrollDirection: Axis.vertical,
+            children: List.generate(foods.length, (i) {
+              var food = foods[i];
+              return FoodTile(food: food);
+            }),
+          ),
+        ),
+      ),
     );
   }
 }
