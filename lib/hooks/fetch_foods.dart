cdc9b9b232cd79b6f59effdb4410f89776c5a3ad
Apoorva-Saxena-hub <saxenapoorva1307@gmail.com>
2025-08-18 23:30:38 +0530

    changes

---
+++ lib/hooks/fetch_foods.dart
@@ -0,0 +1,51 @@
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:foodly_app/constants/constants.dart';
+import 'package:foodly_app/models/api_error.dart';
+import 'package:foodly_app/models/food.dart';
+import 'package:foodly_app/models/hook_model/hook_result.dart';
+import 'package:foodly_app/models/restaurant.dart';
+import 'package:http/http.dart' as http;
+
+FetchHook useFetchFoods(String code) {
+  final foods = useState<List<FoodsModel>?>(null);
+  final isLoading = useState<bool>(false);
+  final error = useState<Exception?>(null);
+  final apiError = useState<ApiError?>(null);
+
+  Future<void> fetchData() async {
+    isLoading.value = true;
+
+    try {
+      Uri url = Uri.parse('$baseUrl/api/foods/recommendation/$code');
+      final response = await http.get(url);
+      print(response.statusCode);
+
+      if (response.statusCode == 200) {
+        foods.value = foodsModelFromJson(response.body);
+      } else {
+        apiError.value = apiErrorFromJson(response.body);
+      }
+    } catch (e) {
+      error.value = e as Exception;
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  useEffect(() {
+    fetchData();
+    return null;
+  }, []);
+
+  void refetch() {
+    isLoading.value = true;
+    fetchData();
+  }
+
+  return FetchHook(
+    data: foods.value,
+    isLoading: isLoading.value,
+    error: error.value,
+    refetch: refetch,
+  );
+}
--- lib/views/home/widgets/food_list.dart
+++ lib/views/home/widgets/food_list.dart
@@ -1,29 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:foodly_app/common/shimmers/nearby_shimmer.dart';
 import 'package:foodly_app/constants/constants.dart';
 import 'package:foodly_app/constants/uidata.dart';
+import 'package:foodly_app/hooks/fetch_foods.dart';
+import 'package:foodly_app/models/food.dart';
 import 'package:foodly_app/views/home/widgets/food_widget.dart';
 
-class FoodList extends StatelessWidget {
+class FoodList extends HookWidget {
   const FoodList({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final hookResults = useFetchFoods("41007428");
+    List<FoodsModel>? foods = hookResults.data;
+    final isLoading = hookResults.isLoading;
     return Container(
       height: 184.h,
       padding: EdgeInsets.only(left: 12.w, top: 10.h),
-      child: ListView(
-        scrollDirection: Axis.horizontal,
-        children: List.generate(foods.length, (i) {
-          var food = foods[i];
-          return FoodWidget(
-            image: food['imageUrl'],
-            title: food['title'],
-            time: food['time'],
-            price: food['price'].toStringAsFixed(2),
-          );
-        }),
-      ),
+      child: isLoading
+          ? const NearbyShimmer()
+          : ListView(
+              scrollDirection: Axis.horizontal,
+              children: List.generate(foods!.length, (i) {
+                FoodsModel food = foods[i];
+                return FoodWidget(
+                  image: food.imageUrl[0],
+                  title: food.title,
+                  time: food.time,
+                  price: food.price.toStringAsFixed(2),
+                );
+              }),
+            ),
     );
   }
 }
